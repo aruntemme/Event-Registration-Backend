bce62ae5c07f076225a70b8f74da57bbbf5b5887
Arun K <arungauthamk@gmail.com>
2021-09-26 15:48:00 +0530

    search events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,7 +6,7 @@ module Api
     
       # GET /events
       def index
-        @events = Event.all
+        @events = Event.order("id DESC").all
     
         render json: @events
       end
@@ -15,13 +15,18 @@ module Api
       def show
         render json: @event
       end
-    
+      
+      def search
+        @event = Event.search(params[:email])
+        render json:  {status: 'success', event: @event}, status: :created
+      end
+
       # POST /events
       def create
         @event = Event.new(event_params)
     
         if @event.save
-          render json: @event, status: :created, location: @event
+          render json:  {status: 'success', event: @event}, status: :created
         else
           render json: @event.errors, status: :unprocessable_entity
         end
@@ -30,7 +35,7 @@ module Api
       # PATCH/PUT /events/1
       def update
         if @event.update(event_params)
-          render json: @event
+          render json: {status: 'success', event: @event}
         else
           render json: @event.errors, status: :unprocessable_entity
         end
@@ -39,6 +44,7 @@ module Api
       # DELETE /events/1
       def destroy
         @event.destroy
+        render json: {status: 'success'}
       end
     
       private
@@ -49,7 +55,7 @@ module Api
     
         # Only allow a list of trusted parameters through.
         def event_params
-          params.require(:event).permit(:title, :description, :duration, :fees, :location, :date, :tags, :maxparticipants, :configurefields)
+          params.require(:event).permit(:title, :description, :duration, :fees, :location, :date, :tags, :maxparticipants, :configurefields, :createdby)
         end
     end
   end
